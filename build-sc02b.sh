f560c87c36107a084262a06f0f6858d4f436d3cf
didhiy <didhiy@gmail.com>
2013-03-22 06:58:03 +0700

    New build setup

--- build-sc02b.sh
+++ build-sc02b.sh
@@ -6,26 +6,20 @@ DATE_START=$(date +"%s")
 
 make "cyanogenmod_galaxys_sc02b_defconfig"
 
-eval $(grep CONFIG_INITRAMFS_SOURCE .config)
-INIT_DIR=$CONFIG_INITRAMFS_SOURCE
-MODULES_DIR=`echo $INIT_DIR`files/modules
 KERNEL_DIR=`pwd`
 OUTPUT_DIR=../output
-CWM_DIR=../ramdisk-sc02b/cwm/
+CWM_DIR=../ramdisk-sc02b/mackay/
+MODULES_DIR=../ramdisk-sc02b/mackay/system/lib/modules/
 
-echo "INIT_DIR="$INIT_DIR
-echo "MODULES_DIR="$MODULES_DIR
 echo "KERNEL_DIR="$KERNEL_DIR
 echo "OUTPUT_DIR="$OUTPUT_DIR
 echo "CWM_DIR="$CWM_DIR
+echo "MODULES_DIR="$MODULES_DIR
 
-make modules
+make
 
 rm `echo $MODULES_DIR"/*"`
 find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
-
-make zImage
-
 cp arch/arm/boot/zImage $CWM_DIR"boot.img"
 cd $CWM_DIR
 zip -r `echo $BUILDVERSION`.zip *
