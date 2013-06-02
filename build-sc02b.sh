cbe2fe8acdcce034893b09527125fbacf0f90495
didhiy <didhiy@gmail.com>
2013-06-03 01:18:19 +0700

    Revert "Use modified stock ramdisk and rename to "nutest""
    This reverts commit a6b4783a73779983270f9597f3a9ab51a5d76bd6.
    
    Conflicts:
    
    	source/usr/galaxysmtd_initramfs_files/ramdisk-recovery.img
    	source/usr/galaxysmtd_initramfs_files/ramdisk.img

--- build-sc02b.sh
+++ build-sc02b.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
+echo
 echo "Making JB-4.2/CM-10.1 kernel for GS1 NTT-Docomo SC-02B"
-BUILDVERSION=nutest_sc02b-`date +%m%d`
+BUILDVERSION=nitest_sc02b-`date +%m%d`
 DATE_START=$(date +"%s")
 
 make "cyanogenmod_galaxys_sc02b_defconfig"
 
+eval $(grep CONFIG_INITRAMFS_SOURCE .config)
+INIT_DIR=$CONFIG_INITRAMFS_SOURCE
+MODULES_DIR=`echo $INIT_DIR`files/modules
 KERNEL_DIR=`pwd`
 OUTPUT_DIR=../output
-CWM_DIR=../ramdisk-sc02b/stock/
-MODULES_DIR=../ramdisk-sc02b/stock/system/lib/modules/
+CWM_DIR=../ramdisk-sc02b/cwm/
 
+echo "INIT_DIR="$INIT_DIR
+echo "MODULES_DIR="$MODULES_DIR
 echo "KERNEL_DIR="$KERNEL_DIR
 echo "OUTPUT_DIR="$OUTPUT_DIR
 echo "CWM_DIR="$CWM_DIR
-echo "MODULES_DIR="$MODULES_DIR
 
-make
+make modules
 
 rm `echo $MODULES_DIR"/*"`
 find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+
+make zImage
+
 cp arch/arm/boot/zImage $CWM_DIR"boot.img"
 cd $CWM_DIR
 zip -r `echo $BUILDVERSION`.zip *
